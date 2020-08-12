0e73dc2197f35f0a452c540a9ecbe6e38b157c03
Kaushik Kulkarni <kaushikcfd@gmail.com>
2020-08-12 15:42:00 -0500

    define VecCreate{Seq|MPI}ViennaCLWithArrays

--- src/vec/vec/impls/mpi/mpiviennacl/mpiviennacl.cxx
+++ src/vec/vec/impls/mpi/mpiviennacl/mpiviennacl.cxx
@@ -326,6 +326,86 @@ PetscErrorCode  VecCreateMPIViennaCLWithArray(MPI_Comm comm,PetscInt bs,PetscInt
   PetscFunctionReturn(0);
 }
 
+/*@C
+   VecCreateMPIViennaCLWithArrays - Creates a parallel, array-style vector,
+   where the user provides the ViennaCL vector to store the vector values.
+
+   - if only cpuarray is NULL, then memory for the CPU data is allocated.
+   - if only viennaclvec is NULL, then memory for the underlying ViennaCL vector is allocated.
+   - if both cpuarray and viennaclvec are provided, the caller must ensure that the provided arrays have identical values.
+   - if both cpuarray and viennaclvec are NULL, then both are allocated.
+
+   Collective
+
+   Input Parameters:
++  comm  - the MPI communicator to use
+.  bs    - block size, same meaning as VecSetBlockSize()
+.  n     - local vector length, cannot be PETSC_DECIDE
+.  N     - global vector length (or PETSC_DECIDE to have calculated)
+-  cpuarray - the user provided CPU array to store the vector values
+-  viennaclvec - ViennaCL vector where the Vec entries are to be stored on the device.
+
+   Output Parameter:
+.  vv - the vector
+
+   Notes:
+   Use VecDuplicate() or VecDuplicateVecs() to form additional vectors of the
+   same type as an existing vector.
+
+   PETSc does NOT free the provided arrays when the vector is destroyed via
+   VecDestroy(). The user should not free the array until the vector is
+   destroyed.
+
+   Level: intermediate
+
+.seealso: VecCreateSeqViennaCLWithArrays(), VecCreateMPIWithArray()
+          VecCreate(), VecDuplicate(), VecDuplicateVecs(), VecCreateGhost(),
+          VecCreateMPI(), VecCreateGhostWithArray(), VecViennaCLPlaceArray(),
+          VecPlaceArray(), VecCreateMPICUDAWithArrays()
+
+@*/
+PetscErrorCode  VecCreateMPIViennaCLWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,PetscInt N,const PetscScalar cpuarray[],const ViennaCLVector *viennaclvec,Vec *vv)
+{
+  PetscErrorCode ierr;
+
+  PetscFunctionBegin;
+  ierr = VecCreateMPIViennaCLWithArray(comm,bs,n,N,viennaclvec,vv);CHKERRQ(ierr);
+
+  if (viennaclvec && cpuarray)
+  {
+    Vec_MPI *s         = (Vec_MPI*)((*vv)->data);
+    s->array           = (PetscScalar*)cpuarray;
+    (*vv)->offloadmask = PETSC_OFFLOAD_BOTH;
+  }
+  else if (!viennaclvec)
+  {
+    Vec_MPI *s         = (Vec_MPI*)((*vv)->data);
+    s->array           = (PetscScalar*)cpuarray;
+    (*vv)->offloadmask =  PETSC_OFFLOAD_CPU;
+    // allocate device data
+    ierr = VecViennaCLAllocateCheck(*vv); CHKERRQ(ierr);
+  }
+  else if (!cpuarray)
+  {
+    (*vv)->offloadmask = PETSC_OFFLOAD_GPU;
+    // allocate host data
+    ierr = VecViennaCLAllocateCheckHost(*vv); CHKERRQ(ierr);
+  }
+  else
+  {
+    // allocate device data
+    ierr = VecViennaCLAllocateCheck(*vv); CHKERRQ(ierr);
+    // allocate host data
+    ierr = VecViennaCLAllocateCheckHost(*vv); CHKERRQ(ierr);
+    ierr = VecSet(*vv,0.0);CHKERRQ(ierr);
+    ierr = VecSet_Seq(*vv,0.0);CHKERRQ(ierr);
+    (*vv)->offloadmask = PETSC_OFFLOAD_BOTH;
+  }
+
+  PetscFunctionReturn(0);
+}
+
+
 PetscErrorCode VecCreate_MPIViennaCL_Private(Vec vv,PetscBool alloc,PetscInt nghost,const ViennaCLVector *array)
 {
   PetscErrorCode ierr;
--- src/vec/vec/impls/seq/seqviennacl/vecviennacl.cxx
+++ src/vec/vec/impls/seq/seqviennacl/vecviennacl.cxx
@@ -1084,6 +1084,83 @@ PETSC_EXTERN PetscErrorCode  VecCreateSeqViennaCLWithArray(MPI_Comm comm,PetscIn
 }
 
 /*@C
+   VecCreateSeqViennaCLWithArrays - Creates a ViennaCL sequential vector, where
+   the user provides the array space to store the vector values.
+
+   - if only cpuarray is NULL, then memory for the CPU data is allocated.
+   - if only viennaclvec is NULL, then memory for the underlying ViennaCL vector is allocated.
+   - if both cpuarray and viennaclvec are provided, the caller must ensure that the provided arrays have identical values.
+   - if both cpuarray and viennaclvec are NULL, then both are allocated.
+
+   Collective
+
+   Input Parameter:
++  comm - the communicator, should be PETSC_COMM_SELF
+.  bs - the block size
+.  n - the vector length
+-  cpuarray - CPU memory where the vector elements are to be stored.
+-  viennaclvec - ViennaCL vector where the Vec entries are to be stored on the device.
+
+   Output Parameter:
+.  V - the vector
+
+   Notes:
+   PETSc does NOT free the provided arrays when the vector is destroyed via
+   VecDestroy(). The user should not free the array until the vector is
+   destroyed.
+
+   Level: intermediate
+
+.seealso: VecCreateMPIViennaCLWithArrays(), VecCreate(), VecCreateSeqWithArray(),
+          VecViennaCLPlaceArray(), VecPlaceArray(), VecCreateSeqCUDAWithArrays()
+@*/
+PetscErrorCode  VecCreateSeqViennaCLWithArrays(MPI_Comm comm,PetscInt bs,PetscInt n,const PetscScalar cpuarray[],const ViennaCLVector* viennaclvec,Vec *V)
+{
+  PetscErrorCode ierr;
+  PetscMPIInt    size;
+
+  PetscFunctionBegin;
+
+  ierr = MPI_Comm_size(comm,&size);CHKERRQ(ierr);
+  if (size > 1) SETERRQ(PETSC_COMM_SELF,PETSC_ERR_ARG_WRONG,"Cannot create VECSEQ on more than one process");
+
+  // set V's viennaclvec to be viennaclvec, do not allocate memory on host yet.
+  ierr = VecCreateSeqViennaCLWithArray(comm,bs,n,viennaclvec,V);CHKERRQ(ierr);
+
+  if (viennaclvec && cpuarray)
+  {
+    Vec_Seq *s = (Vec_Seq*)((*V)->data);
+    s->array = (PetscScalar*)cpuarray;
+    (*V)->offloadmask = PETSC_OFFLOAD_BOTH;
+  }
+  else if (!viennaclvec)
+  {
+    Vec_Seq *s = (Vec_Seq*)((*V)->data);
+    s->array = (PetscScalar*)cpuarray;
+    (*V)->offloadmask = PETSC_OFFLOAD_CPU;
+    // allocate device data
+    ierr = VecViennaCLAllocateCheck(*V); CHKERRQ(ierr);
+  }
+  else if (!cpuarray)
+  {
+    (*V)->offloadmask = PETSC_OFFLOAD_GPU;
+    // allocate host data
+    ierr = VecViennaCLAllocateCheckHost(*V); CHKERRQ(ierr);
+  }
+  else
+  {
+    ierr = VecViennaCLAllocateCheck(*V); CHKERRQ(ierr);
+    ierr = VecViennaCLAllocateCheckHost(*V); CHKERRQ(ierr);
+    ierr = VecSet(*V,0.0);CHKERRQ(ierr);
+    ierr = VecSet_Seq(*V,0.0);CHKERRQ(ierr);
+    (*V)->offloadmask = PETSC_OFFLOAD_BOTH;
+  }
+
+  PetscFunctionReturn(0);
+}
+
+
+/*@C
    VecViennaCLPlaceArray - Replace the viennacl vector in a Vec with
    the one provided by the user. This is useful to avoid a copy.
 
